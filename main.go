7dd8affee52849d734787dbef6f416d41c48ca4f
Sean Anih <sean.anih@gmail.com>
2023-03-18 22:20:52 -0400

    tested more complex functions

--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func main() {
-	sayMyName("")
-	fmt.Println(printArray("yesyouare"))
+	fmt.Println(printArray("cbac"))
+
+	cycleNames([]string{"Sean", "Programmer", "Cold"}, sayMyName)
+
+	showRadius(3.5)
 }
 
 func sayMyName(name string) {
 	if len(name) == 0 {
 		fmt.Println("Nothin to say...")
 	} else {
-		fmt.Printf("Wuz hannen, %v!", name)
+		fmt.Printf("Wuz hannen, %v! \n", name)
 	}
 }
 
@@ -25,14 +29,29 @@ func printArray(text string) string {
 
 	textSlice := strings.Split(text, "")
 	myMap := make(map[string]uint8)
+	var result string
 
 	for i := 0; i < len(textSlice); i++ {
 		if myMap[textSlice[i]] == 0 {
 			myMap[textSlice[i]] = 1
-		} else {
-			return fmt.Sprintf("the repeated value is: %v", textSlice[i])
+			continue
+		}
+
+		if myMap[textSlice[i]] == 1 {
+			result = fmt.Sprintf("the first repeated value is: %v", textSlice[i])
+			break
 		}
 	}
 
-	return ""
+	return result
+}
+
+func cycleNames(names []string, f func(string)) {
+	for i := 0; i < len(names); i++ {
+		f(names[i])
+	}
+}
+
+func showRadius(r float32) {
+	fmt.Println("Radius is: ", math.Pi*r*r)
 }
